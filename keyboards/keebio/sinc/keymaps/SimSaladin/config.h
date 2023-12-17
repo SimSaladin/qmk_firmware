d2e61bde65d6bb3acd54ace642a0817adacc2f68
Samuli Thomasson <samuli.thomasson@pm.me>
2023-12-17 22:03:23 +0100

    [SimSaladin] update sinc rev.2 km

---
+++ keyboards/keebio/sinc/keymaps/SimSaladin/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Mouse Keys (kinetic mode)
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_DELAY         0
+#define MOUSEKEY_INTERVAL      1
+#define MOUSEKEY_MOVE_DELTA    10
+#define MOUSEKEY_INITIAL_SPEED 350
