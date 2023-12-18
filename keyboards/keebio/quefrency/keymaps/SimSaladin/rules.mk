38a9b96913f3187da369f35313543318443c844f
Samuli Thomasson <samuli.thomasson@pm.me>
2023-12-18 20:07:59 +0100

    [SimSaladin] add quefrency keymap

---
+++ keyboards/keebio/quefrency/keymaps/SimSaladin/rules.mk
@@ -0,0 +1,6 @@
+# Build options
+MOUSEKEY_ENABLE = yes
+#EXTRAKEY_ENABLE = yes    # Use system and audio control key codes
+#TAP_DANCE_ENABLE = yes    # Use multi-tap features
+#UNICODEMAP_ENABLE = yes
+NKRO_ENABLE = yes
